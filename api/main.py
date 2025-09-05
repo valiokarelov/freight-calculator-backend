81860f87d5faa50ff19d28d7ce6dc8b834336db5
Valentin <valentin.k.stoyanov@gmail.com>
2025-09-05 07:50:25 -0500

    Fixing main.py file after moved under api

--- api/main.py
+++ api/main.py
@@ -52,4 +52,4 @@ def calculate_chargeable_weight(request: ChargeableWeightRequest):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("api.main:app", host="0.0.0.0", port=port, reload=True)
